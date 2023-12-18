ab3cb4c41898ed23c612826da558c6d15157ce79
12siragi <siragisaidi218@gmaol.com>
2023-12-18 15:01:13 -0800

    inter

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,21 +3,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    """Prints an integer with "{:d}".format().
-
-    If a ValueError message is caught, a corresponding
-    message is printed to standard error.
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
-    except (TypeError, ValueError) as e:
+    except ValueError as e:
         print(f"Exception: {e}", file=sys.stderr)
         return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of a list that are integers.
+    count = 0
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(int(my_list[i])), end=" ")
+                count += 1
+            except (ValueError, TypeError):
+                continue
+    except IndexError:
+        pass  # Expected exception when x is greater than the length of the list
 
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-
-    Returns:
-        The number of elements printed.
-    """
-    ret = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return ret
-
-# Example usage:
-sample_list = [1, 2, 'a', 4, 'b', 6]
-num_printed = safe_print_list_integers(sample_list, 4)
-print(f"Number of integers printed: {num_printed}")
+    print()  # New line after printing integers
+    return count
