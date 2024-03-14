738b2e7187b8e063579c7b520a95fcc37ea7f360
12siragi <siragisaid218@gmail.com>
2024-03-14 07:14:28 +0000

    moresql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Select cities of California using subquery
+SELECT * FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id;
+
