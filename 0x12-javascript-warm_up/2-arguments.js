4e2335b7b74c1af50c873d2fde223912bd67e8fe
12siragi <siragisaidi218@gmail.com>
2024-04-08 20:40:46 +0000

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2); // Removes the first two elements
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
+
