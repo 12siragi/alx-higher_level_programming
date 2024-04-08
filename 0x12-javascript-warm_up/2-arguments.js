2eb135226256191da817aaa2a68934bf0e23bdcd
12siragi <siragisaidi218@gmail.com>
2024-04-08 21:12:43 +0000

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-/* script that prints a message depending of the number of arguments passed */
-const arg = process.argv.length;
-if (arg <= 2) {
+
+const count = process.argv.length;
+
+if (count === 2) {
   console.log('No argument');
-}
-if (arg === 3) {
+} else if (count === 3) {
   console.log('Argument found');
-}
-if (arg > 3) {
+} else {
   console.log('Arguments found');
