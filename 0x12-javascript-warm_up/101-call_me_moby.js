77350ce0ecece829467ee52a02c1589b8ded0594
12siragi <siragisaidi218@gmail.com>
2024-04-10 01:20:12 +0000

    script

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
-};
+
+// Define the callMeMoby function
+function callMeMoby(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+// Export the callMeMoby function to make it visible outside
+module.exports.callMeMoby = callMeMoby;
+
