9e465a021d7fa9dfad2d963d9c9fd8a17a56b53e
12siragi <siragisaidi218@gmail.com>
2024-04-10 01:06:27 +0000

    script

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+
+// Define the add function
+function add(a, b) {
+  return a + b;
+}
+
+// Export the add function to make it visible outside
+module.exports.add = add;
+
