66bdf1aed66fe13573ec0aab085333e430c62c05
12siragi <siragisaidi218@gmail.com>
2024-04-27 19:43:52 +0000

    all

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request to the URL using curl, and displays the body of the response for a 200 status code
-curl -s -o /tmp/body.txt -w "%{http_code}" "$1"
-status_code=$(cat /tmp/body.txt)
-if [[ $status_code -eq 200 ]]; then
-    curl -s "$1"
-fi
-rm -f /tmp/body.txt
+# displays body of 200 status code response
+curl -sL $1
